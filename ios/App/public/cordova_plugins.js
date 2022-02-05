425fc4bf74b5a2a8090851fc68d5bb2b74287512
Denan Golos <denan.golos@vitecsoftware.com>
2022-02-05 10:07:30 +0100

    Merge

---
+++ ios/App/public/cordova_plugins.js
@@ -0,0 +1,61 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "cordova-plugin-nativestorage.mainHandle",
+          "file": "plugins/cordova-plugin-nativestorage/www/mainHandle.js",
+          "pluginId": "cordova-plugin-nativestorage",
+        "clobbers": [
+          "NativeStorage"
+        ]
+        },
+      {
+          "id": "cordova-plugin-apprate.AppRate",
+          "file": "plugins/cordova-plugin-apprate/www/AppRate.js",
+          "pluginId": "cordova-plugin-apprate",
+        "clobbers": [
+          "AppRate",
+          "window.AppRate"
+        ]
+        },
+      {
+          "id": "cordova-plugin-apprate.locales",
+          "file": "plugins/cordova-plugin-apprate/www/locales.js",
+          "pluginId": "cordova-plugin-apprate",
+        "runs": true
+        },
+      {
+          "id": "cordova-plugin-apprate.storage",
+          "file": "plugins/cordova-plugin-apprate/www/storage.js",
+          "pluginId": "cordova-plugin-apprate",
+        "runs": true
+        },
+      {
+          "id": "cordova-plugin-nativestorage.LocalStorageHandle",
+          "file": "plugins/cordova-plugin-nativestorage/www/LocalStorageHandle.js",
+          "pluginId": "cordova-plugin-nativestorage"
+        },
+      {
+          "id": "cordova-plugin-nativestorage.NativeStorageError",
+          "file": "plugins/cordova-plugin-nativestorage/www/NativeStorageError.js",
+          "pluginId": "cordova-plugin-nativestorage"
+        },
+      {
+          "id": "cordova-plugin-dialogs.notification",
+          "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+          "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+          "navigator.notification"
+        ]
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "cordova-plugin-apprate": "1.7.2",
+      "cordova-plugin-dialogs": "2.0.2",
+      "cordova-plugin-nativestorage": "2.3.2"
+    };
+    // BOTTOM OF METADATA
+    });
+    
